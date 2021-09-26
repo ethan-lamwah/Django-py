510c5a603d969ecd5eaf1ee064a6f08566491a79
Ethan Lam <hellowah5@gmail.com>
2021-09-26 18:43:06 +0800

    add a Word Counter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
+    hour = datetime.now().hour
+    greeting = "Good morning" if 5<=hour<12 else "Good afternoon" if hour<18 else "Good evening"
+    
     context = {
-        'greeting' : 'Good morning',
+        'greeting' : greeting,
         'name' : 'Ethan'
     }
     return render(request, 'index.html', context)
     # return HttpResponse('<h1>Hello world!</h1>') 
+    
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount' : amount_of_words})
