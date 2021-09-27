8056132577c6964d6fb25e0096609b4b4139301a
Ethan Lam <hellowah5@gmail.com>
2021-09-28 00:37:39 +0800

    POST method in Words Counter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -15,6 +15,6 @@ def index(request):
     # return HttpResponse('<h1>Hello world!</h1>') 
     
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount' : amount_of_words})
