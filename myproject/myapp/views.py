04fad9c277f75883789e3cdf18fe3d233bf057f3
Ethan Lam <hellowah5@gmail.com>
2021-09-28 23:26:44 +0800

    add and use  models.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import Feature
 
 # Create your views here.
 def index(request):
     hour = datetime.now().hour
     greeting = "Good morning" if 5<=hour<12 else "Good afternoon" if hour<18 else "Good evening"
     
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'This is the details'
+    
     context = {
         'greeting' : greeting,
-        'name' : 'Ethan'
+        'name' : 'Ethan',
+        'feature1' : feature1
     }
     return render(request, 'index.html', context)
     # return HttpResponse('<h1>Hello world!</h1>') 
